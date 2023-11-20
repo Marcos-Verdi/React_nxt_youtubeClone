fc3bc7b2eb7022cb63ed3092e2370ec5335aa266
Marcos Verdi <70574457+Marcos-Verdi@users.noreply.github.com>
2023-11-20 17:27:35 -0600

    commit 11/20

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import PageHeader from '@/components/PageHeader'
+
+export default function Home() {
+  return (
+    <div className='max-h-screen flex flex-col'>
+      <PageHeader />
+      <div>2</div>
+    </div>
+  )
+}
